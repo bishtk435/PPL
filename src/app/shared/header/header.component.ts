794bd6619b5e255f00768cdcc101912a2421ff10
bishtk435 <bishtk435@gmail.com>
2020-09-11 15:36:29 +0530

    Resolved header component bug

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { isLoogedIn } from 'src/app/app.reducer';
 
 @Component({
   selector: 'app-header',
@@ -10,17 +11,21 @@ import { Observable } from 'rxjs';
 })
 export class HeaderComponent implements OnInit {
 
-  isLoggedIn$: Observable<boolean>;
-  // isLoggedIn = false;
+  //  isLoggedIn$: Observable<boolean>;
+
+  loggedIn = null;
 
   constructor(private store: Store<{LoggedIn: boolean}>, private router: Router) {
-    this.isLoggedIn$ = store.pipe(select('LoggedIn'));
+  //  this.isLoggedIn$ = store.pipe(select('LoggedIn'));
    }
 
   ngOnInit(): void {
+    this.loggedIn = localStorage.getItem('email');
   }
 
-
+  ngDoCheck(): void {
+    this.loggedIn = localStorage.getItem('email');
+  }
 
   goToHome(): void{
     this.router.navigate(['/home-page']);
